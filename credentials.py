25259efe5fa77000ce87f5a760ab26da174dfaf3
adam916 <adam.ziebe@gmail.com>
2022-11-03 17:45:41 +0100

    Delete credentials.py

--- credentials.py
+++
@@ -1,9 +0,0 @@
-credentials = {
-    'ssid' : 'LTE-1758', 
-    'password' : '12345678',
-    'ADAFRUIT_IO_URL' : b'io.adafruit.com',
-    'ADAFRUIT_USERNAME' : b'adam8143',
-    'ADAFRUIT_IO_KEY' : b'aio_DXMt313tQgd3vEgvFELhTC9DzqNV',
-    'ADAFRUIT_IO_FEEDNAME' : b'iotfeed'
-    }
-
